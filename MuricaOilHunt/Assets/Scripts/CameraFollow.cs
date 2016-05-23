cbeb597ffd39f6c8681acda9ef6141609a4b4ac6
Albin Frisk Pettersson <frisal@utb.fuv.se>
2016-05-23 13:49:30 +0200

    Camera followscript added

---
+++ MuricaOilHunt/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform mPlayer;
+    [SerializeField]
+    private Vector3 offset;
+
+    void Update()
+    {
+        transform.position = new Vector3(mPlayer.position.x + offset.x, mPlayer.position.y + offset.y, offset.z); // Camera follows the mPlayer with specified offset position
+    }
+}
