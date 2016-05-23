531820be0540caa73fe6c0c0539890773aa4e2e3
Glänn <hallda@3se-spel-17.utb.fuv.se>
2016-05-23 13:28:27 +0200

    Created Core for player and enemy

---
+++ MuricaOilHunt/Assets/Scripts/BaseUnit.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+SCRIPT HANDLES BASE STRUCTURE FOR PLAYER AND ENEMY VALUES
+CAN BE USED FOR INHERTIANCE OF CERTAIN BEAHVIOUR or CHANGE OF BEHAVIOUR
+-- Created by David Halldin
+*/
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BaseUnit : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0, 100)] private float mHealth;
+
+    protected Rigidbody2D mRigidBody;
+    public GameObject mPlayer;
+    public GameObject mEnemy;
+
+    void Awake()
+    {
+        mPlayer = GameObject.FindGameObjectWithTag("Player");
+        mEnemy = GameObject.FindGameObjectWithTag("Enemy");
+    } 
+
+    void Start ()
+    {
+       	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(mHealth <= 0)
+        {
+            //insert value and action here
+        }
+	}
+}
