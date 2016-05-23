0677b12924defe94c86502a66802b8249312dc85
Albin Frisk Pettersson <frisal@utb.fuv.se>
2016-05-23 13:07:50 +0200

    menu script

---
+++ MuricaOilHunt/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour
+{
+
+	void Update ()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Application.LoadLevel("Scene_1");
+            Debug.Log("asd");
+        }
+	
+	}
+}
